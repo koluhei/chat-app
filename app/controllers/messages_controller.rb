aac84cf14925e863d144963f1b5fe47d970bf74d
koluhei <qpal79@gmail.com>
2021-07-05 21:20:27 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
       redirect_to room_messages_path(@room)
     else
       render :index
+      @messages = @room.messages.includes(:user)
     end
   end
 
